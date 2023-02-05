09c312867ad9249162f1e49ef1eadb244d686c54
Liam Kinne <liam@kinne.io>
2023-02-05 17:42:50 +1000

    add device identifiers

---
+++ src/device.rs
@@ -0,0 +1,26 @@
+enum Device {
+    VehicleController,
+    SteeringWheel,
+    ArrayIsolationController,
+    WaveSculptor,
+    MpptA,
+    MpptB,
+}
+
+fn device_source_address(device: Device) -> Option<u8> {
+    match device {
+        Device::VehicleController => Some(0x10),
+        Device::SteeringWheel => Some(0x20),
+        Device::ArrayIsolationController => Some(0x30),
+        _ => None
+    }
+}
+
+fn device_base_address(device: Device) -> Option<u16> {
+    match device {
+        Device::WaveSculptor => Some(0x400),
+        Device::MpptA => Some(0x600),
+        Device::MpptB => Some(0x610),
+        _ => None,
+    }
+}
