f6ba30ba1d80dde852d17fa81d500efd823d2cce
Liam Kinne <liam@kinne.io>
2023-03-09 00:28:37 +1000

    add other priorities

--- src/com/mod.rs
+++ src/com/mod.rs
@@ -11,10 +11,28 @@ pub mod startup;
 /// We will fill these in 0 to 7 as necesarry.
 #[derive(Default, Copy, Clone)]
 pub enum Priority {
+    /// Messages that may be safety critical in delivery.
     Critical = 0,
+
+    /// Messages that should have guarenteed delivery.
     Important = 1,
+
+    /// Messages used to assert control.
     Control = 2,
+
+    /// Messages used for notifications.
+    Notification = 3,
+
+    /// Messages used to report status.
+    Status = 4,
+
+    // Not in use
+    // Reserved = 5,
+
+    /// Default message priority.
     #[default]
     Default = 6,
+
+    /// Unimportant messages.
     Background = 7,
 }
