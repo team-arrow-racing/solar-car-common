1817a12b4ee2380ec5e086a2654e4c97e213b577
Liam Kinne <liam@kinne.io>
2023-03-09 00:23:58 +1000

    add priority values

--- src/com/mod.rs
+++ src/com/mod.rs
@@ -2,3 +2,19 @@
 
 pub mod heartbeat;
 pub mod startup;
+
+/// Message priority.
+/// 
+/// These are mostly used to control message arbitration and not necessarily
+/// ever read.
+/// 
+/// We will fill these in 0 to 7 as necesarry.
+#[derive(Default, Copy, Clone)]
+pub enum Priority {
+    Critical = 0,
+    Important = 1,
+    Control = 2,
+    #[default]
+    Default = 6,
+    Background = 7,
+}
