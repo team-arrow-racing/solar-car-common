4620e2345abe5c42e63e4078136c28cda44675a2
Ben Harvey <88410750+benharv4@users.noreply.github.com>
2023-08-28 18:31:01 +1000

    Abstracted out horn enum, pgn, and message fn.

---
+++ src/com/horn.rs
@@ -0,0 +1,27 @@
+use crate::com::MessageFormat;
+use crate::device::{source_address, Device};
+use bitflags::bitflags;
+use bxcan::{ExtendedId, Frame};
+use j1939::pgn::{Number, Pgn};
+
+#[repr(u8)]
+pub enum HornMessageFormat {
+    Enable = 0xEE,
+}
+
+pub const PGN_HORN_MESSAGE: Number = Number {
+    specific: device::Device::VehicleController as u8,
+    format: HornMessageFormat::Enable as u8,
+    data_page: false,
+    extended_data_page: false,
+};
+
+pub fn horn_message(device: Device, state: u8) -> Frame {
+    let id = j1939::ExtendedId {
+        priority: Priority::Default as u8,
+        pgn: Pgn::new(PGN_HORN_MESSAGE),
+        source_address: source_address(device).unwrap(),
+    };
+
+    Frame::new_data(ExtendedId::new(id.to_bits()).unwrap(), [state])
+}
